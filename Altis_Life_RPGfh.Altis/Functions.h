48ff8980424530db66663f32368eafa5697f780c
Isoterik <Isoterik@gmx.net>
2015-01-18 01:50:07 +0100

    überflüssig

--- Altis_Life_RPGfh.Altis/Functions.h
+++ Altis_Life_RPGfh.Altis/Functions.h
@@ -380,8 +380,7 @@ class Life_Client_Core
 		class fedCamDisplay {};
 		class copLoadout {};
 		class ticketPaid {};
-		class buyCevlar {};		
-		class cam {};
+		class buyCevlar {};
 		class markVictim {};
 	};
 
