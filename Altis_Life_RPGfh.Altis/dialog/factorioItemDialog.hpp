e6487ee3df6794898ff3f8b6be608f7f384aa3da
Isoterik <Isoterik@gmx.net>
2015-01-22 00:59:57 +0100

    kleinigkeit :)

--- Altis_Life_RPGfh.Altis/dialog/factorioItemDialog.hpp
+++ Altis_Life_RPGfh.Altis/dialog/factorioItemDialog.hpp
@@ -93,7 +93,7 @@ class FactorioItemDialog {
 			text = "";
 			sizeEx = 0.04;
 			colorBackground[] = {0.1,0.1,0.1,0.9};
-			onLBSelChanged = "_this call life_fnc_vehicleShopLBChange";
+			onLBSelChanged = "[] call life_fnc_vehicleShopLBChange";
 			
 			//Position & height
 			x = 0.11; y = 0.302;
